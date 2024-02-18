cb28bf3cf6005cabbb3311ef4fe52ed641c61917
Sankalp Tiwari <41205888+SankalpSTiwari@users.noreply.github.com>
2024-02-17 23:21:42 -0800

    Update background color

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,32 +1,31 @@
 /* eslint-disable react/no-unused-state */
 /* eslint-disable no-console */
 /* eslint-disable */
-import React, { PureComponent } from "react";
+import React, { PureComponent } from 'react';
 import { Redirect } from 'react-router';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../../App.css";
-import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
-import Button from "react-bootstrap/Button";
-import pic from "../../gym1.jpg";
-import logo from "../../logo.jpg";
-import axios from "axios";
-import url from "../../utils/urlconfig";
-import jwtDecode from "jwt-decode";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../App.css';
+import { MDBContainer, MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import Button from 'react-bootstrap/Button';
+import pic from '../../gym1.jpg';
+import logo from '../../logo.jpg';
+import axios from 'axios';
+import url from '../../utils/urlconfig';
+import jwtDecode from 'jwt-decode';
 
 class Signup extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      useremail: "",
-      usercontact: "",
-      userweight: "",
-      userheight: "",
-      password: ""
+      username: '',
+      useremail: '',
+      usercontact: '',
+      userweight: '',
+      userheight: '',
+      password: '',
       //redirect: "/"
     };
   }
-
 
   // username change handler to update state variable with the text entered by the user
   usernameChangeHandler = (e) => {
@@ -48,7 +47,6 @@ class Signup extends PureComponent {
       usercontact: e.target.value,
     });
   };
-
 
   // weight change handler to update state variable with the text entered by the user
   weightChangehandler = (e) => {
@@ -78,15 +76,18 @@ class Signup extends PureComponent {
       contact: this.state.usercontact,
       weight: this.state.userweight,
       height: this.state.userheight,
-      password: this.state.password
+      password: this.state.password,
     };
     axios({
       url: `${url}/user/signup`,
-      method: "post",
+      method: 'post',
       data: data,
     })
       .then(async (response) => {
-        await localStorage.setItem('currentUser', JSON.stringify(response.data.jwt));
+        await localStorage.setItem(
+          'currentUser',
+          JSON.stringify(response.data.jwt)
+        );
         //setting admin value 1: is admin, 0: not an admin
         //await localStorage.setItem('admin', jwtDecode(JSON.stringify(response.data.jwt)).isadmin);
         //getting admin value from local storage and printing it!
@@ -94,11 +95,11 @@ class Signup extends PureComponent {
         //var isadmin = localStorage.getItem('admin');
         if (response.status === 200) {
           await this.setState({
-            redirect: "/userdashboard",
+            redirect: '/userdashboard',
           });
         }
       })
-      .catch(function (error) {
+      .catch(function(error) {
         alert(error);
       });
   };
@@ -109,105 +110,107 @@ class Signup extends PureComponent {
       redirectVar = <Redirect to={this.state.redirect} />;
     }
     return (
-      <div>{redirectVar}
-      <MDBContainer className="my-5 gradient-form">
-        <MDBRow>
-          <MDBCol col="6" className="mb-5">
-            <div className="d-flex flex-column ms-5">
-              <div className="text-center">
-                <img src={logo} style={{ width: "185px" }} alt="logo" />
-                <h1 className="bookheading">Start your fitness journey with us</h1>
-              </div>
-              <br></br>
+      <div className='signup-page' style={{ backgroundColor: '#272626' }}>
+        {redirectVar}
+        <MDBContainer className='my-5 gradient-form'>
+          <MDBRow>
+            <MDBCol col='6' className='mb-5'>
+              <div className='d-flex flex-column ms-5'>
+                <div className='text-center'>
+                  <img src={logo} style={{ width: '185px' }} alt='logo' />
+                  <h1 className='bookheading'>
+                    Start your fitness journey with us!
+                  </h1>
+                </div>
+                <br></br>
 
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="username"
-                  placeholder="Full Name"
-                  onChange={this.usernameChangeHandler}
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="email"
-                  className="form-control"
-                  name="useremail"
-                  placeholder="user@example.com"
-                  onChange={this.emailChangehandler}
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="usercontact"
-                  placeholder="contact no"
-                  onChange={this.contactChangehandler}
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="userweight"
-                  placeholder="weight in lbs"
-                  onChange={this.weightChangehandler}
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="userheight"
-                  placeholder="height in cms"
-                  onChange={this.heightChangehandler}
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="password"
-                  className="form-control"
-                  name="password"
-                  placeholder="password"
-                  onChange={this.passwordChangeHandler}
-                />
-              </div>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    name='username'
+                    placeholder='Full Name'
+                    onChange={this.usernameChangeHandler}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='email'
+                    className='form-control'
+                    name='useremail'
+                    placeholder='user@example.com'
+                    onChange={this.emailChangehandler}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    name='usercontact'
+                    placeholder='contact no'
+                    onChange={this.contactChangehandler}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    name='userweight'
+                    placeholder='weight in lbs'
+                    onChange={this.weightChangehandler}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    name='userheight'
+                    placeholder='height in cms'
+                    onChange={this.heightChangehandler}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    className='form-control'
+                    name='password'
+                    placeholder='password'
+                    onChange={this.passwordChangeHandler}
+                  />
+                </div>
 
-              <div className="text-center pt-1 mb-5 pb-1">
-                <Button variant="danger" onClick={this.registerUser}>
-                  Sign Me Up!
-                </Button>
-              </div>
+                <div className='text-center pt-1 mb-5 pb-1'>
+                  <Button variant='danger' onClick={this.registerUser}>
+                    Sign Me Up!
+                  </Button>
+                </div>
 
-              <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
-                <p className="large fw-bold mt-2 pt-1 mb-2">
-                  Already have an account?{" "}
-                  <a href="/login" className="link-danger">
-                    <b>Sign In!</b>
-                  </a>
-                </p>
+                <div className='d-flex flex-row align-items-center justify-content-center pb-4 mb-4'>
+                  <p className='large fw-bold mt-2 pt-1 mb-2'>
+                    Already have an account?{' '}
+                    <a href='/login' className='link-danger'>
+                      <b>Sign In!</b>
+                    </a>
+                  </p>
+                </div>
               </div>
-            </div>
-          </MDBCol>
+            </MDBCol>
 
-          <MDBCol col="6" className="mb-5">
-            <div className="d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4">
-              <div className="text-white px-3 py-4 p-md-5 mx-md-4">
-                <h4 className="mb-4">
-                  Need to track your gym activities? We got it all
-                  covered!{" "}
-                </h4>
-                <p className="small mb-0">
-                  Register with us to start your fitness journey!!
-                </p>
-                <img src={pic} style={{ width: "485px" }} alt="logo" />
+            <MDBCol col='6' className='mb-5'>
+              <div className='d-flex flex-column  justify-content-center gradient-custom-2 h-100 mb-4'>
+                <div className='text-white px-3 py-4 p-md-5 mx-md-4'>
+                  <h4 className='mb-4'>
+                    Need to track your gym activities? We got it all covered!{' '}
+                  </h4>
+                  <p className='small mb-0'>
+                    Register with us to start your fitness journey!!
+                  </p>
+                  <img src={pic} style={{ width: '485px' }} alt='logo' />
+                </div>
               </div>
-            </div>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
       </div>
     );
   }
