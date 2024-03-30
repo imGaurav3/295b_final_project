cb71a97107e8d1a7294367198102ecd4ffb5f30a
Ashish Gaurav <ashish.g308@gmail.com>
2024-03-30 15:47:18 -0700

    Added db details

--- backend/routes/config.js
+++ backend/routes/config.js
@@ -1,10 +1,10 @@
 const dbConfig = {
     connectionLimit: 20, // default 10
-    host: 'dbcmpe202.csd8irggvxdu.us-west-2.rds.amazonaws.com',
+    host: 'dbfinalproject.cpcem4w6a78y.us-east-1.rds.amazonaws.com',
     port: '3306',
     user: 'admin',
-    password: '202apyyproject',
-    database: 'dbhealthclub',
+    password: '295bfinalproject',
+    database: 'finaldb',
   };
 
 module.exports = dbConfig;
